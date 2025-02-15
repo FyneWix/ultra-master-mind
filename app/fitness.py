aefd5857f3d042e72a3a114ed9743f08bd281e41
Fyne <mary.e2204591@etud.univ-ubs.fr>
2025-02-14 21:34:39 -0500

    Refactor fitness function and add selection module for chromosome selection

--- app/fitness.py
+++ app/fitness.py
@@ -1,11 +1,10 @@
-def fitness(C, PM):
+def fitness_value(C, PM):
     """
     C: a chromosome
     PM: the mystery phrase
 
     return the fitness value of the chromosome
     """
-
     # Check if the chromosome and the mystery phrase have the same length
     if len(C) != len(PM):
         raise ValueError("Chromosome and mystery phrase must have the same length")
@@ -14,3 +13,12 @@ def fitness(C, PM):
     for i in range(0, len(PM)):
         sum += abs(ord(C[i]) - ord(PM[i]))
     return -sum
+
+def fitness_list(population, PM):
+    """
+    population: list of chromosomes
+    PM: the mystery phrase
+
+    return a list a tuples of the form (chromosome, fitness value)
+    """
+    return [(chromosome, fitness(chromosome, PM)) for chromosome in population]
---
+++ app/selection.py
@@ -0,0 +1,32 @@
+from app.fitness import fitness_list
+
+def sort_fitness_list(population_with_fitness):
+    """
+    population_with_fitness: list of tuples of the form (chromosome, fitness value)
+
+    return a list of tuples sorted by fitness value in descending order
+    """
+    return sorted(population_with_fitness, key=lambda x: x[1], reverse=True)
+
+
+def extract_chromosomes(population_with_fitness):
+    """
+    population_with_fitness: list of tuples of the form (chromosome, fitness value)
+
+    return a list of chromosomes sorted by fitness value in descending order
+    """
+    return [chromosome for chromosome, fitness_value in population_with_fitness]
+
+def selection(population, TS, N, PM):
+    """
+    population: list of chromosomes
+    TS: selection rate
+    N: size of the population
+    PM: the mystery phrase
+
+    return a list of chromosomes selected from the population
+    """
+    population_with_fitness = fitness_list(population, PM)
+    sorted_population_with_fitness = sort_fitness_list(population_with_fitness)
+    best_chromosomes = extract_chromosomes(sorted_population_with_fitness)
+    return best_chromosomes[:int(TS * N)]
