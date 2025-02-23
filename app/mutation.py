8a5ee6b291efbb0e618acfa7e9f6d3b306794a1e
Fyne <mary.e2204591@etud.univ-ubs.fr>
2025-02-22 23:39:24 -0500

    Remove debug print statements

--- app/mutation.py
+++ app/mutation.py
@@ -13,28 +13,22 @@ def mutation(population, MR):
 
     # Selection of unique indexes for the chromosomes to mutate
     index_list = random.sample(range(N), n_mutations)
-    print("Index list: ", index_list)
 
     for index in index_list:
         # Convert the chromosome into a list of characters
         chromosome = list(population[index])
-        print("Chromosome: ", chromosome)
 
         # Selection of a random character index in the chromosome
         gene_index = random.randint(0, len(chromosome) - 1)
-        print("Gene index: ", gene_index)
 
         # New character to replace the old one
         new_char = chr(random.randint(32, 126))
-        print("New char: ", new_char)
 
         # Replace the old character by the new one
         chromosome[gene_index] = new_char
-        print("Chromosome after mutation: ", chromosome)
 
         # Convert the list of characters back to a string and update the population
         population[index] = ''.join(chromosome)
-        print("Population after mutation: ", population)
 
     return population
 
